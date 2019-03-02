c796b91582d741df447ab55abd8144e4fd602f7c
Jin <peng0442@gmail.com>
2019-03-02 15:27:43 -0600

    camera

--- racing game/racing/Assets/Scripts/CameraStable.cs
+++ racing game/racing/Assets/Scripts/CameraStable.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class CameraStable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject TheCar;
+    public float CarX;
+    public float CarY;
+    public float CarZ;
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        CarX = TheCar.transform.eulerAngles.x;
+        CarY = TheCar.transform.eulerAngles.y;
+        CarZ = TheCar.transform.eulerAngles.z;
+
+        transform.eulerAngles = new Vector3(CarX - CarX, CarY, CarZ - CarZ);
+
     }
 }
---
+++ racing game/racing/Assets/Scripts/HalfPointTrigger.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalfPointTrigger : MonoBehaviour
+{
+    public GameObject LapCompleteTrig;
+    public GameObject HalfLapTrig;
+
+    void On
+}
